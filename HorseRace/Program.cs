c6ffe46ae3cc99f517f6b02c2814c7d672868699
Claudio Moraes <claudio.moraes@automate.com.br>
2020-06-26 23:55:30 -0300

    'initial'

---
+++ HorseRace/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+namespace HorseRace
+{
+    partial class Program
+    {
+        static AutoResetEvent finishLine = new AutoResetEvent(false);
+        static int count = 0;
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                ThreadPool.QueueUserWorkItem(new WaitCallback(Horse1), finishLine);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(Horse2), finishLine);
+                ThreadPool.QueueUserWorkItem(new WaitCallback(Horse3), finishLine);
+
+                finishLine.WaitOne();
+
+                Console.WriteLine(count.ToString());
+
+                Console.ReadKey();
+            }
+        }
+
+        static void Horse1(object stateInfo)
+        {
+            Console.WriteLine("Start galloping.");
+
+            // Simulate time spent working.
+            Thread.Sleep(new Random().Next(100, 2000));
+
+            // Signal that work is finished.
+            count = 1;
+            ((AutoResetEvent)stateInfo).Set();
+        }
+
+        static void Horse2(object stateInfo)
+        {
+            Console.WriteLine("Start galloping.");
+
+            // Simulate time spent working.
+            Thread.Sleep(new Random().Next(100, 2000));
+
+            // Signal that work is finished.
+            count = 2;
+            ((AutoResetEvent)stateInfo).Set();
+        }
+
+        static void Horse3(object stateInfo)
+        {
+            Console.WriteLine("Start galloping.");
+
+            // Simulate time spent working.
+            Thread.Sleep(new Random().Next(100, 2000));
+
+            // Signal that work is finished.
+            count = 3;
+            ((AutoResetEvent)stateInfo).Set();
+        }
+    }
+}
